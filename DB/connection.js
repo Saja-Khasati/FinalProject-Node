ad5e79e881fb330303ddc7f133022b84bdd61997
Saja-Khasati <106661799+Saja-Khasati@users.noreply.github.com>
2024-01-21 23:44:40 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   console.log("DB Connection String:", "mongodb+srv://saja:SajaKha2001@atlascluster.5lummgr.mongodb.net/finalProject");
 
   try {
-    await mongoose.connect("mongodb+srv://saja:SajaKha2001@atlascluster.5lummgr.mongodb.net/finalProject", { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect("mongodb+srv://saja:SajaKha2001@atlascluster.5lummgr.mongodb.net/finalProject", { useUnifiedTopology: true });
     console.log("DB connection established");
   } catch (error) {
     console.error(`Error connecting to the database: ${error}`);
